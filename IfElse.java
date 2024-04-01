602013b8e7eb65df50fc0bfbcfad0bebfe8b90d4
The_Pi$ith <pisith@Pisiths-MacBook-Pro.local>
2024-04-01 16:47:58 +0700

    implemented If-Else with simple example

---
+++ IfElse.java
@@ -0,0 +1,13 @@
+class IfElse {
+	public static void main (String[] args) {
+		// int x = 4;
+		int x = 3;
+		if (x == 3) {
+			System.out.println("This is will print only X is equal 3");
+		}
+		else {
+			System.out.println("This is will print only X is not equal 3");
+		}
+		System.out.println("This is will print no matter what");
+	}
+}
